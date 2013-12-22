f4604b94ff9f8bb5a0843d0fc3b2c5199f9df19f
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-12-22 15:00:51 +0200

    Methods to find and manipulate orders

--- pyramidpayment/models.py
+++ pyramidpayment/models.py
@@ -22,13 +22,39 @@ class Order(Base):
     __tablename__ = 'order'
     id = Column(Integer,
                 Sequence('order_seq_id', optional=True),
-                primary_key=True,
-               )
-    order_number = Column(Text, unique=True)
+                primary_key=True,)
+    external_reference_number = Column(Text)
     value = Column(Integer)
     status = Column(Text)
 
-    def __init__(self, order_number, value):
-        self.order_number = order_number
+    def __init__(self, external_reference_number='', value=0):
+        self.external_reference_number = external_reference_number
         self.value = value
-        self.status = 'ordered'
+        self.status = 'order_pending'
+    
+    def formatted_order_number(self):
+        """ Format the id by prepending the string 'order_' and left padding the
+            actual id up to 4 characters with '0'.
+            e.g. An order with the id '1' will be formatted:
+            'order_0001'
+        """
+        if self.id is None:
+            return ''
+        return 'order_{id:04}'.format(id=self.id)
+    
+    def as_dict(self):
+        return dict(
+            order_number = self.formatted_order_number(),
+            value = self.value, 
+            external_reference_number = self.external_reference_number,
+        )
+
+    @classmethod
+    def by_order_number(self, order_number):
+        order = DBSession.query(Order).filter(Order.order_number == order_number)
+        return order.first()
+
+    @classmethod
+    def next_order_number(self, order_number):
+        results = DBSession.execute('select max("id") from "order"')
+        return results.first()[0] +1
