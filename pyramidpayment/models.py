7ba2dfc5181e94c2f395e9cf32b00b32eb7359e2
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-12-26 14:13:55 +0200

    Add license text and update model

--- pyramidpayment/models.py
+++ pyramidpayment/models.py
@@ -1,3 +1,31 @@
+##############################################################################
+#
+# Copyright (c) 2013 Rijk Stofberg
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the MIT License (MIT),
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+#
+##############################################################################
+
 from sqlalchemy import (
     Column,
     Sequence,
@@ -23,13 +51,15 @@ class Order(Base):
     id = Column(Integer,
                 Sequence('order_seq_id', optional=True),
                 primary_key=True,)
-    external_reference_number = Column(Text)
+    description = Column(Text)
     value = Column(Integer)
+    external_reference_number = Column(Text)
     status = Column(Text)
 
-    def __init__(self, external_reference_number='', value=0):
-        self.external_reference_number = external_reference_number
+    def __init__(self, description, value, external_reference_number=''):
+        self.description = description
         self.value = value
+        self.external_reference_number = external_reference_number
         self.status = 'order_pending'
     
     def formatted_order_number(self):
@@ -44,8 +74,9 @@ class Order(Base):
     
     def as_dict(self):
         return dict(
-            order_number = self.formatted_order_number(),
+            description = self.description,
             value = self.value, 
+            order_number = self.formatted_order_number(),
             external_reference_number = self.external_reference_number,
         )
 
