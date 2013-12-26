aaa12992ffcecefdfc2da8808f4164fd9cef617a
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-12-26 14:13:26 +0200

    Add confirm route

--- pyramidpayment/__init__.py
+++ pyramidpayment/__init__.py
@@ -17,6 +17,7 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('order', '/order')
+    config.add_route('confirm', '/confirm')
     config.add_route('list_orders', '/list_orders')
     config.scan()
     return config.make_wsgi_app()
